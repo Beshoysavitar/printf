48a65cfa444015969ff9b173bd4f7cb73539afc4
beshoy <besho.elking33377@gmail.com>
2023-04-25 08:06:09 -0700

    error

--- easy_printers.c
+++ easy_printers.c
@@ -12,7 +12,7 @@ int pr_rot(va_list ap, para *poo)
 {
 	char a[] =
 		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
-	char *a = va_arg(ap, char *);
+	char *ar = va_arg(ap, char *);
 	int i, j;
 	int c = 0;
 	(void)poo;
@@ -26,7 +26,7 @@ int pr_rot(va_list ap, para *poo)
 		    || (a[i] >= 'a' && a[i] <= 'z'))
 		{
 			j = a[i] - 65;
-			c += _putchar(a[j]);
+			c += _putchar(ar[j]);
 		}
 		else
 			c += _putchar(a[i]);
@@ -45,12 +45,13 @@ int pr_rot(va_list ap, para *poo)
  */
 int pr_fr_to(char *sa, char *so, char *ex)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; sa <= so; sa++)
+	while (sa <= so)
 	{
 		if (sa != ex)
-		i += _putchar(*sa);
+			i += _putchar(*sa);
+		sa++;
 	}
 	return (i);
 }
@@ -64,31 +65,17 @@ int pr_fr_to(char *sa, char *so, char *ex)
  */
 int pr_rev(va_list ap, para *poo)
 {
-	(void)poo;
 	int len, i = 0;
 	char *st = va_arg(ap, char *);
-
-	switch ((int)(!*st))
-	{
-		case 1:
-			*st = NULL_S;
-	}
+	(void)poo;
 
 	if (st)
 	{
-		len = 0;
-		while (*st)
-		{
+		for (len = 0; *st; st++)
 			len++;
-			st++;
-		}
 		st--;
-		while (len > 0)
-		{
+		for (; len > 0; len--, st--)
 			i += _putchar(*st);
-			st--;
-			len--;
-		}
 	}
 	return (i);
 }
